d6ad40f153cf654cce9fed812a97d8c7eb8606a6
kate-deriv <kate@regentmarkets.com>
2025-02-12 22:29:20 +0300

    feat: 1-14 add pipe for posts validation

--- src/features/bloggers-platform/api/input-dto/posts.input-dto.ts
+++ src/features/bloggers-platform/api/input-dto/posts.input-dto.ts
@@ -1,13 +1,52 @@
+import { Transform } from 'class-transformer';
+import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
+
 export class CreatePostInputDto {
+  @IsString()
+  @Transform(({ value }: { value?: string | null }) => value?.trim())
+  @IsNotEmpty()
+  @MaxLength(30)
   title: string;
+
+  @IsString()
+  @Transform(({ value }: { value?: string | null }) => value?.trim())
+  @IsNotEmpty()
+  @MaxLength(100)
   shortDescription: string;
+
+  @IsString()
+  @Transform(({ value }: { value?: string | null }) => value?.trim())
+  @IsNotEmpty()
+  @MaxLength(1000)
   content: string;
+
+  @IsString()
+  @Transform(({ value }: { value?: string | null }) => value?.trim())
+  @IsNotEmpty()
   blogId: string;
 }
 
 export class UpdatePostInputDto {
+  @IsString()
+  @Transform(({ value }: { value?: string | null }) => value?.trim())
+  @IsNotEmpty()
+  @MaxLength(30)
   title: string;
+
+  @IsString()
+  @Transform(({ value }: { value?: string | null }) => value?.trim())
+  @IsNotEmpty()
+  @MaxLength(100)
   shortDescription: string;
+
+  @IsString()
+  @Transform(({ value }: { value?: string | null }) => value?.trim())
+  @IsNotEmpty()
+  @MaxLength(1000)
   content: string;
+
+  @IsString()
+  @Transform(({ value }: { value?: string | null }) => value?.trim())
+  @IsNotEmpty()
   blogId: string;
 }
